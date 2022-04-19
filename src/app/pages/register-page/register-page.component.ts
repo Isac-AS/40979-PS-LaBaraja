08d2d27fed28c88b624adbfdad959660218804a9
raulmat19 <91469476+raulmat19@users.noreply.github.com>
2022-04-19 22:56:29 +0100

    Register password matching not finished

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,22 +1,44 @@
-import { Router} from "@angular/router";
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+import { AbstractControl, FormBuilder, FormControl, FormGroupDirective, NgForm, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { AuthService} from "../../services/auth.service";
-import { CustomUtilsService} from "../../services/customUtils.service";
+import { AuthService } from "../../services/auth.service";
+import { CustomUtilsService } from "../../services/customUtils.service";
+import { group } from "console";
+import { ErrorStateMatcher } from "@angular/material/core";
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control?.invalid && control?.parent?.dirty);
+    const invalidParent = !!(control?.parent?.invalid && control?.parent?.dirty);
+
+    return invalidCtrl || invalidParent;
+  }
+}
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
+
 export class RegisterPageComponent implements OnInit {
+
+  matcher = new ErrorStateMatcher();
+
+  checkPasswords: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    let pass = group.get('password')?.value;
+    let confirmPass = group.get('alt_password')?.value;
+    console.log(pass);
+    console.log(confirmPass);
+    return pass === confirmPass ? null : {notSame: true}
+  }
 
   newUser = this.fb.group({
     email : ['', [Validators.required, Validators.email]],
     username : [ '', [Validators.required, Validators.minLength(6)]],
     password : ['', [Validators.required, Validators.minLength(6)]],
-    alt_password : ['', [Validators.required, Validators.minLength(6)]]
-  });
+    alt_password : ['', [Validators.required]]
+  }, { validators: this.checkPasswords });
 
   constructor(
     private router: Router,
