40006764ba28dbf4b1b61605f5782f4280ee7f49
Gilson Constantino Dias de Carvalho <a10190@alunos.cv.unipiaget.org>
2022-04-22 11:31:47 +0100

    add routing main

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { PresentationPageComponent } from './pages/presentation-page/presentation-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 const routes: Routes = [
   {path: '', component: PresentationPageComponent},
   {path: 'home', component: HomePageComponent},
   {path: 'login', component: LoginPageComponent},
   {path: 'register', component: RegisterPageComponent},
+  {path: 'main', component: MainPageComponent},
 ];
 
 @NgModule({
