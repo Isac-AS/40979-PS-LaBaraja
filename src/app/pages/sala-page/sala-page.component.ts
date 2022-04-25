0d7b3b7ac3d2ad0c4ae30c94d0c0c4fe521c56af
isac <isac.as0001@gmail.com>
2022-04-25 12:33:58 +0100

    Pre-model modification commit

--- src/app/pages/sala-page/sala-page.component.ts
+++ src/app/pages/sala-page/sala-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { User } from 'firebase/auth';
+import { NotificationDialogComponent } from 'src/app/components/notification-Dialog/notification-Dialog.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { databaseService } from 'src/app/services/database.service';
 
 @Component({
   selector: 'app-sala-page',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalaPageComponent implements OnInit {
 
-  constructor() { }
+  currentUserData: User |undefined;
+
+  constructor(
+    public dialog: MatDialog,
+    private auth: AuthService,
+    private db: databaseService,
+  ) { 
+    this.auth.getUid().then(async currentUserUid => {
+      if (currentUserUid){
+        this.db.readDocument<User>('users', currentUserUid).subscribe( async currentUserData => {
+          this.currentUserData = currentUserData;
+        })
+      }
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  openNotificationDialog(): void {
+    const dialogRef = this.dialog.open(NotificationDialogComponent, {
+      width: "70%",
+      height: "70%"
+    });
+    dialogRef.afterClosed().subscribe(res => {});
   }
 
 }
