f719b0bc4cdc9444fb2a32e21c1b890befb90c8e
juanmamed <91466489+juanmamed@users.noreply.github.com>
2022-05-03 12:09:33 +0100

    Fixed Body Styling and created usefull absolute-positioned header

--- src/app/pages/sala-page/lobby-page.component.ts
+++ src/app/pages/sala-page/lobby-page.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { take, takeLast } from 'rxjs';
 import { NotificationDialogComponent } from 'src/app/components/notification-Dialog/notification-Dialog.component';
 import { FriendInfo, Lobby, User } from 'src/app/models/interfaces';
 import { AuthService } from 'src/app/services/auth.service';
@@ -28,33 +27,21 @@ export class LobbyPageComponent implements OnInit {
     friendList: [],
     inbox: [],
     lobby: '',
-    shortNameId: ''
   };
-
-  onChange: boolean = true;
 
   constructor(
     public dialog: MatDialog,
     private auth: AuthService,
     private db: databaseService,
-    private changeDetection: ChangeDetectorRef
   ) {
     this.auth.getUid().then(async currentUserUid => {
       if (currentUserUid){
         this.db.readDocument<User>('users', currentUserUid).subscribe( async currentUserData => {
-          console.log("aaaaa");
           if (currentUserData) this.currentUserData = currentUserData;
           if (currentUserData?.lobby != 'none') {
             this.db.readDocument<Lobby>('lobbies', currentUserData!?.lobby).subscribe( async usersLobby => {
               if(usersLobby) this.currentLobby = usersLobby;
-              console.log(this.onChange);
-              console.log(this.currentLobby.participants.length);
             })
-          }
-
-          else {
-            this.clearCurrentLobby();
-            this.changeDetection.detectChanges();
           }
         })
       }
@@ -77,12 +64,12 @@ export class LobbyPageComponent implements OnInit {
     this.db.createDocument<Lobby>(lobby, 'lobbies', lobby.id);
     this.currentUserData.lobby = lobby.id
     this.db.updateDocument<User>(this.currentUserData, 'users', this.currentUserData.uid)
+
   }
 
   quitLobby() {
     this.db.removeFromLobby(this.currentLobby, {id: this.currentUserData.uid, name: this.currentUserData.name});
     this.clearCurrentLobby();
-    this.onChange = false;
   }
 
   removeFromLobby(friend: FriendInfo) {
@@ -110,6 +97,8 @@ export class LobbyPageComponent implements OnInit {
       id: '',
       participants : []
     }
+    console.log(this.currentLobby.participants[0]);
   }
+
   
 }
